636dfb2fb4cf74a2a9ef70257cb34c3d6ece040f
Leissan Khan <leissan.khan@gmail.com>
2022-01-16 10:42:20 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name: "Leissan",
+    streetAddress: "Astoria, NY",
+  };
+
+  function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+  
+    newObj[key] = value;
+  
+    return newObj;
+  }
+  
+  const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Sam"
+  );
+  
+  newEmployee.name;
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+  
+  const newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+  
+  newEmployee.streetAddress;
